a982678a2c9f2f59f0257bc6120f3771a9efe60a
Emanuel Lucas <elsa2@aluno.ifal.edu.br>
2024-12-26 22:47:47 -0300

    feat: add category entity

---
+++ src/main/java/br/com/emanuel/catalog/entities/Category.java
@@ -0,0 +1,49 @@
+package br.com.emanuel.catalog.entities;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Category implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private long id;
+    private String name;
+
+    public Category() {
+    }
+
+    public Category(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return id == category.id && Objects.equals(name, category.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
