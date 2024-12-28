59aa2ea18da9c4f6ae6424b14bb5769c9505286c
Emanuel Lucas <elsa2@aluno.ifal.edu.br>
2024-12-28 20:55:01 -0300

    feat: h2 database

---
+++ src/main/java/br/com/emanuel/catalog/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package br.com.emanuel.catalog.repositories;
+
+import br.com.emanuel.catalog.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
--- src/main/java/br/com/emanuel/catalog/resources/CategoryResource.java
+++ src/main/java/br/com/emanuel/catalog/resources/CategoryResource.java
@@ -1,23 +1,25 @@
 package br.com.emanuel.catalog.resources;
 
 import br.com.emanuel.catalog.entities.Category;
+import br.com.emanuel.catalog.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 
+    @Autowired
+    private CategoryService service;
+
     @GetMapping
     public ResponseEntity<List<Category>> findAll() {
-        List<Category> list = new ArrayList<>();
-        list.add(new Category(1L, "Books"));
-        list.add(new Category(2L, "Electronics"));
-        return ResponseEntity.ok().body(list);
+        List<Category> categories = service.findAll();
+        return ResponseEntity.ok().body(categories);
     }
 }
