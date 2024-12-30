6e2ef401be9758bf294587361417482087d44240
Emanuel Lucas <elsa2@aluno.ifal.edu.br>
2024-12-30 00:11:04 -0300

    feat: add pagination

--- src/main/java/br/com/emanuel/catalog/resources/CategoryResource.java
+++ src/main/java/br/com/emanuel/catalog/resources/CategoryResource.java
@@ -3,12 +3,14 @@ package br.com.emanuel.catalog.resources;
 import br.com.emanuel.catalog.dto.CategoryDTO;
 import br.com.emanuel.catalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categories")
@@ -18,8 +20,14 @@ public class CategoryResource {
     private CategoryService service;
 
     @GetMapping
-    public ResponseEntity<List<CategoryDTO>> findAll() {
-        List<CategoryDTO> categories = service.findAll();
+    public ResponseEntity<Page<CategoryDTO>> findAll(
+        @RequestParam(value = "page", defaultValue = "0") Integer page,
+        @RequestParam(value = "linesPerPage", defaultValue = "15") Integer linesPerPage,
+        @RequestParam(value = "orderBy", defaultValue = "name") String orderBy,
+        @RequestParam(value = "direction", defaultValue = "ASC") String direction
+    ) {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        Page<CategoryDTO> categories = service.findAllPaged(pageRequest);
         return ResponseEntity.ok().body(categories);
     }
 
--- src/main/java/br/com/emanuel/catalog/services/CategoryService.java
+++ src/main/java/br/com/emanuel/catalog/services/CategoryService.java
@@ -7,14 +7,14 @@ import br.com.emanuel.catalog.exceptions.ResourceNotFoundException;
 import br.com.emanuel.catalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
@@ -23,11 +23,9 @@ public class CategoryService {
     private CategoryRepository repository;
 
     @Transactional(readOnly = true)
-    public List<CategoryDTO> findAll() {
-        List<Category> categories = repository.findAll();
-        return categories.stream()
-                .map(CategoryDTO::new)
-                .collect(Collectors.toList());
+    public Page<CategoryDTO> findAllPaged(PageRequest pageRequest) {
+        Page<Category> categories = repository.findAll(pageRequest);
+        return categories.map(CategoryDTO::new);
     }
 
     @Transactional(readOnly = true)
