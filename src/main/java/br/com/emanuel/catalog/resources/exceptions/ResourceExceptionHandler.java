fc74c57db825ead3ca3e1024d9cb1bc59a5020a5
Emanuel Lucas <elsa2@aluno.ifal.edu.br>
2024-12-29 22:30:23 -0300

    feat: add update category method

--- src/main/java/br/com/emanuel/catalog/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/br/com/emanuel/catalog/resources/exceptions/ResourceExceptionHandler.java
@@ -1,6 +1,6 @@
 package br.com.emanuel.catalog.resources.exceptions;
 
-import br.com.emanuel.catalog.exceptions.EntityNotFoundException;
+import br.com.emanuel.catalog.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,8 +12,8 @@ import java.time.Instant;
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
-    @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<StandardError> entityNotFound(EntityNotFoundException ex, HttpServletRequest request) {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException ex, HttpServletRequest request) {
         StandardError err = new StandardError();
         err.setTimestamp(Instant.now());
         err.setStatus(HttpStatus.NOT_FOUND.value());
--- src/main/java/br/com/emanuel/catalog/services/CategoryService.java
+++ src/main/java/br/com/emanuel/catalog/services/CategoryService.java
@@ -2,12 +2,13 @@ package br.com.emanuel.catalog.services;
 
 import br.com.emanuel.catalog.dto.CategoryDTO;
 import br.com.emanuel.catalog.entities.Category;
-import br.com.emanuel.catalog.exceptions.EntityNotFoundException;
+import br.com.emanuel.catalog.exceptions.ResourceNotFoundException;
 import br.com.emanuel.catalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -29,7 +30,7 @@ public class CategoryService {
     @Transactional(readOnly = true)
     public CategoryDTO findById(Long id) {
         Optional<Category> obj = repository.findById(id);
-        Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+        Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
         return new CategoryDTO(entity);
     }
 
@@ -39,5 +40,17 @@ public class CategoryService {
         entity.setName(dto.getName());
         entity = repository.save(entity);
         return new CategoryDTO(entity);
+    }
+
+    @Transactional
+    public CategoryDTO update(Long id, CategoryDTO dto) {
+        try {
+            Category entity = repository.getOne(id);
+            entity.setName(dto.getName());
+            entity = repository.save(entity);
+            return new CategoryDTO(entity);
+        } catch (EntityNotFoundException ex) {
+            throw new ResourceNotFoundException("ID not found " + id);
+        }
     }
 }
